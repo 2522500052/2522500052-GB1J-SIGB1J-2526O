694da7550f27d6f5578ece8cdf4e66e5c7a80861
noviana siadari <2522500052@mahasiswa.atmaluhur.ac.id>
2025-12-18 17:20:56 +0700

    menambahkan kode cid di-casting di read.php

--- pertemuan-12/README.md
+++ pertemuan-12/README.md
@@ -4,5 +4,6 @@ NIM  : 2522500052 <br>
 KEL  : SI-GB1J <br>
 <ol>
 <li>mengcopy file pertmuan 11 ke pertemuan 12</li>
+<li>menambahkan kode cid di-casting di read.php</li>
 </ol >
 
--- pertemuan-12/read.php
+++ pertemuan-12/read.php
@@ -18,6 +18,7 @@ $no      = 1;
     <?php while ($row = mysqli_fetch_assoc($q)): ?>
     <tr>
         <td><?= $no++; ?></td>
+        <td><a href="edit.php?cid=<?= (int)$row['cid']; ?>"Edit</a></td>
         <td><?= $row['cid']; ?></td>
         <td><?= htmlspecialchars($row['cnama']); ?></td>
         <td><?= htmlspecialchars($row['cemail']); ?></td>
